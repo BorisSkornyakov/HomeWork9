d7b7fa411766b0077a5a6f6a16f2b97977b67cb1
BorisSkornyakov <3601124@gmail.com>
2022-09-28 11:20:39 +0500

    Задача2

---
+++ Task2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 66: Задайте значения M и N. 
+// Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// M = 1; N = 15-> 120
+// M = 4; N = 8. -> 30
+
+int DataEntry(string message)                           //Метод ввода данных
+{
+	Console.Write(message);
+	return Convert.ToInt32(Console.ReadLine());
+}
+int SumIntervalNumbers(int n, int m)
+{
+	int trend = 1;
+	if (n<m)
+		trend = -1;
+	if (n == m)
+		return m;
+	return m + SumIntervalNumbers(n, m + trend);
+}
+int num1 = DataEntry("Введите первое число -> ");
+int num2 = DataEntry("Введите второе число -> ");
+System.Console.WriteLine(SumIntervalNumbers(num1, num2));
